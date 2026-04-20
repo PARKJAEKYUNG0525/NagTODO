c7c2d7dc081721a2bc4d50c5be9bcdc12401f22c
PARKJAEKYUNG0525 <pjg0525@naver.com>
2026-04-20 21:14:46 +0900

    crud 비동기 추가

--- backend/app/db/crud/todo.py
+++ backend/app/db/crud/todo.py
@@ -1,57 +1,55 @@
-from sqlalchemy.orm import Session
-from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.future import select
 from app.db.models.todo import Todo
 from app.db.models.user import User
 from app.db.models.category import Category
 from app.db.scheme.todo import TodoCreate, TodoUpdate
-from typing import Optional
 
+class TodoCrud:
 
-# C 생성
-def create_todo(db: Session, data: TodoCreate) -> Todo:
-    todo = Todo(
-        title=data.title,
-        detail=data.detail,
-        todo_status=data.todo_status,
-        visibility=data.visibility,
-        user_id=data.user_id,
-        category_id=data.category_id,
-    )
-    db.add(todo)
-    db.commit()
-    db.refresh(todo)
-    return todo
+    # C 생성
+    @staticmethod
+    async def create_todo(db: AsyncSession, data: TodoCreate) -> Todo:
+        todo = Todo(**data.model_dump())
+        db.add(todo)
+        await db.flush()
+        return todo
 
-# R 조회 - user확인
-def get_user(db: Session, user_id: str) -> Optional[User]:
-    stmt = select(User).where(User.user_id == user_id)
-    return db.scalar(stmt)
+    # R 조회 - user 존재 확인
+    @staticmethod
+    async def get_user(db: AsyncSession, user_id: str) -> User | None:
+        result = await db.execute(select(User).where(User.user_id == user_id))
+        return result.scalar_one_or_none()
 
-# R 조회 - category 존재 확인
-def get_category(db: Session, category_id: str) -> Optional[Category]:
-    stmt = select(Category).where(Category.category_id == category_id)
-    return db.scalar(stmt)
+    # R 조회 - category 존재 확인
+    @staticmethod
+    async def get_category(db: AsyncSession, category_id: str) -> Category | None:
+        result = await db.execute(select(Category).where(Category.category_id == category_id))
+        return result.scalar_one_or_none()
 
-# R 조회 - todo 단일 조회
-def get_todo(db: Session, todo_id: str) -> Optional[Todo]:
-    stmt = select(Todo).where(Todo.todo_id == todo_id)
-    return db.scalar(stmt)
+    # R 조회 - todo 단일 조회
+    @staticmethod
+    async def get_todo(db: AsyncSession, todo_id: str) -> Todo | None:
+        result = await db.execute(select(Todo).where(Todo.todo_id == todo_id))
+        return result.scalar_one_or_none()
 
-# R 조회 - todo 목록 조회 (user 기준)
-def get_all_todos(db: Session, user_id: str) -> list[Todo]:
-    stmt = select(Todo).where(Todo.user_id == user_id)
-    return list(db.scalars(stmt).all())
+    # R 조회 - todo 목록 조회 (user 기준)
+    @staticmethod
+    async def get_all_todos(db: AsyncSession, user_id: str) -> list[Todo]:
+        result = await db.execute(select(Todo).where(Todo.user_id == user_id))
+        return list(result.scalars().all())
 
-# U 수정
-def update_todo(db: Session, todo: Todo, data: TodoUpdate) -> Todo:
-    update_data = data.model_dump(exclude_unset=True)
-    for key, value in update_data.items():
-        setattr(todo, key, value)
-    db.commit()
-    db.refresh(todo)
-    return todo
+    # U 수정
+    @staticmethod
+    async def update_todo(db: AsyncSession, todo: Todo, data: TodoUpdate) -> Todo:
+        update_data = data.model_dump(exclude_unset=True)
+        for key, value in update_data.items():
+            setattr(todo, key, value)
+        await db.flush()
+        return todo
 
-# D 삭제
-def delete_todo(db: Session, todo: Todo) -> None:
-    db.delete(todo)
-    db.commit()
+    # D 삭제
+    @staticmethod
+    async def delete_todo(db: AsyncSession, todo: Todo) -> None:
+        await db.delete(todo)
+        await db.flush()
