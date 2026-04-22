0aaf2eb2678737d10758489668eabaea531395c4
jaehwan <jh990319@naver.com>
2026-04-22 19:49:24 +0900

    pw_history

--- backend/app/db/scheme/pw_history.py
+++ backend/app/db/scheme/pw_history.py
@@ -6,8 +6,17 @@ class PwHistoryBase(BaseModel):
     pw: str
     updated_at: datetime
 
+class PwHistoryCreate(PwHistoryBase):
+    pass
+
+class PwHistoryUpdate(PwHistoryBase):
+    pass
+
 class PwHistoryInDB(PwHistoryBase):
     pw_history_id: str
 
     class Config:
         from_attributes = True
+
+class PwHistoryRead(PwHistoryInDB):
+    pass
